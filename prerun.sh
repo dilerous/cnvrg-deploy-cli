d38a5b5d7353213a981a4fb58b934f5369f0f5b9
Brad Soper <brad.soper@gmail.com>
2022-09-19 21:35:34 -0700

    Update prerun.sh

--- prerun.sh
+++ prerun.sh
@@ -5,6 +5,7 @@ apt-get update
 apt install -y build-essential libssl-dev libreadline-dev zlib1g-dev
 apt install -y rbenv
 rbenv init
+eval "$(rbenv init -)"
 mkdir -p "$(rbenv root)"/plugins
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 rbenv install 2.6.9
