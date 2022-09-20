7e9d6eb4203391b41d6e0426a902ba04fa19f6ce
root <brad.soper@gmail.com>
2022-09-20 04:00:20 +0000

    fixed prerun.sh

--- prerun.sh
+++ prerun.sh
@@ -1,6 +1,7 @@
 apt-get -y install git
 git config --global user.email "brad.soper@gmail.com"
 
+apt-get update
 apt install -y build-essential libssl-dev libreadline-dev zlib1g-dev
 apt install -y rbenv
 rbenv init
@@ -12,3 +13,4 @@ rbenv global 2.6.9
 export FREEDESKTOP_MIME_TYPES_PATH=/cnvrg/freedesktop.org.xml
 gem install cnvrg --no-document
 rbenv rehash
+cnvrg version
